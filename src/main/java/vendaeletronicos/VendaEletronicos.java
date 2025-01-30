91c27a323548d435a622a5dc6427642fb14a8e3c
Franklin Nicolas Wegner <franklinnicolaswegner@gmail.com>
2025-01-30 16:22:08 -0300

    Projeto inicial

---
+++ src/main/java/vendaeletronicos/VendaEletronicos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package vendaeletronicos;
+
+/**
+ *
+ * @author Nicolas
+ */
+public class VendaEletronicos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
